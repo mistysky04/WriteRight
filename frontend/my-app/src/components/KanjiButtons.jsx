40e075ddc48cdcc86dd9cb8491e01decb993542d
Hedie <hediemahmoudian@gmail.com>
2025-03-30 11:02:54 -0700

    Added score

--- frontend/my-app/src/components/KanjiButtons.jsx
+++ frontend/my-app/src/components/KanjiButtons.jsx
@@ -2,14 +2,6 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import '../styling/KanjiButtons.css';
-// import overlayIchi from '../assets/overlay_1ichi.png';
-// import overlayNi from '../assets/overlay_2ni.png';
-// import overlaySan from '../assets/overlay_3san.png';
-// import overlayYon from '../assets/overlay_4yon.png';
-// import overlayGo from '../assets/overlay_5go.png';
-
-
-// Import overlay images
 import overlayIchi from '../assets/overlay_1ichi.png';
 import overlayNi from '../assets/overlay_2ni.png';
 import overlaySan from '../assets/overlay_3san.png';
